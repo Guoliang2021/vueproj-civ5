8518023d778b83c41bd7f47e11c6471a4a59125d
Guoliang2021 <pal32003@163.com>
2022-09-01 14:46:18 +0800

    添加civ5头图

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img class="civ5logo" alt="Civ5 logo" src="../assets/civ5logo.png" />
+    <HelloWorld msg="Welcome to civ5" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 })
 export default class HomeView extends Vue {}
 </script>
+
+<style scoped lang="scss">
+.civ5logo {
+  width: auto;
+  height: auto;
+  max-width: 80%;
+  max-height: 30%;
+}
+</style>
