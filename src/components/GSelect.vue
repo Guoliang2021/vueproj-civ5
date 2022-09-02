47b580c847f75a7507621c85ef85bcaa5433d507
Guoliang2021 <pal32003@163.com>
2022-09-02 17:41:11 +0800

    自定义组件

---
+++ src/components/GSelect.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input readonly v-model="selectedName" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {},
+  computed: {
+    selectedName() {
+      return "xmbo";
+    },
+  },
+})
+export default class GSelect extends Vue {
+  selectedName = undefined;
+}
+</script>
+
+<style lang="scss">
+h1 {
+  font: size 19px;
+}
+</style>
--- src/views/calc/battleDamage.vue
+++ src/views/calc/battleDamage.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>this is battle damage calculate page</h1>
+  <div id="bd-row">
+    <GSelect></GSelect>
+  </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import GSelect from "@/components/GSelect.vue";
+
+@Options({
+  components: {
+    GSelect,
+  },
+})
+export default class BattleDamage extends Vue {}
+</script>
+
+<style lang="scss">
+#bd-row {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+}
+</style>
